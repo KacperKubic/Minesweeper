802fbae258eec04433bf2f3c759615ea5e9eb7bd
KacperKubic <kacper.kubic2211@gmail.com>
2022-09-13 23:52:00 +0200

    Final commit
    I cleared the code and added the comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Variable
 var board = [];
 var mineLocation = [];
 var clicked = 0; 
@@ -22,21 +23,21 @@ window.onload = function(){
     flagButton.addEventListener('click', enableFlagButton, false);
 }
 
-function difficultyEasy()
-{
+//Set difficulty to "easy"
+function difficultyEasy(){
+    //Reset the board
     clicked = 0;
     gameOver = false;
     mineLocation = [];
-    if (document.querySelector('.game_board').classList.contains('medium'))
-    {
-        document.querySelector('.game_board').classList.remove('medium');
-    }
 
-    else if (document.querySelector('.game_board').classList.contains('hard'))
-    {
+    //If the board has got other classes remove them
+    if (document.querySelector('.game_board').classList.contains('medium')){
+        document.querySelector('.game_board').classList.remove('medium');
+    }else if (document.querySelector('.game_board').classList.contains('hard')){
         document.querySelector('.game_board').classList.remove('hard');
     }
-    
+
+    //Define the size, number of mines and difficulty
     board = [];
     document.querySelector('.game_board').classList.add('easy');
     document.getElementById('game_state').innerText = 'Difficulty: Easy';
@@ -44,24 +45,23 @@ function difficultyEasy()
     columns = 8;
     mines = 5;
     difficulty = 1;
-
 }
 
-function difficultyMedium()
-{
+//Set difficulty to "medium"
+function difficultyMedium(){
+    //Reset the board
     clicked = 0;
     gameOver = false;
     mineLocation = [];
-    if (document.querySelector('.game_board').classList.contains('easy'))
-    {
-        document.querySelector('.game_board').classList.remove('easy');
-    }
 
-    else if (document.querySelector('.game_board').classList.contains('hard'))
-    {
+    //If the board has got other classes remove them
+    if (document.querySelector('.game_board').classList.contains('easy')){
+        document.querySelector('.game_board').classList.remove('easy');
+    }else if (document.querySelector('.game_board').classList.contains('hard')){
         document.querySelector('.game_board').classList.remove('hard');
     }
 
+    //Define the size, number of mines and difficulty
     board = [];
     document.querySelector('.game_board').classList.add('medium');
     document.getElementById('game_state').innerText = 'Difficulty: Medium';
@@ -71,21 +71,22 @@ function difficultyMedium()
     difficulty = 2;
 }
 
-function difficultyHard()
-{
+//Set difficulty to "hard
+function difficultyHard(){
+    //Reset the board
     clicked = 0;
     gameOver = false;
     mineLocation = [];
-    if (document.querySelector('.game_board').classList.contains('easy'))
-    {
+
+    //If the board has got other classes remove them
+    if (document.querySelector('.game_board').classList.contains('easy')){
         document.querySelector('.game_board').classList.remove('easy');
     }
-
-    else if (document.querySelector('.game_board').classList.contains('medium'))
-    {
+    else if (document.querySelector('.game_board').classList.contains('medium')){
         document.querySelector('.game_board').classList.remove('medium');
     }
 
+    //Define the size, number of mines and difficulty
     board = [];
     document.querySelector('.game_board').classList.add('hard');
     document.getElementById('game_state').innerText = 'Difficulty: Hard';
@@ -95,38 +96,33 @@ function difficultyHard()
     difficulty = 3;
 }
 
-function addMines()
-{
+//Generate the mines
+function addMines(){
     let minesLeft = mines;
-    while (minesLeft > 0) { 
+    while (minesLeft > 0){ 
         let r = Math.floor(Math.random() * rows);
         let c = Math.floor(Math.random() * columns);
         let id = r.toString() + '-' + c.toString();
-
-        if (!mineLocation.includes(id)) {
+        if (!mineLocation.includes(id)){
             mineLocation.push(id);
             minesLeft -= 1;
         }
     }
 }
 
-
-function createBoard()
-{
+//Create a gameboard based on the difficulty
+function createBoard(){
     document.querySelector('.game_board').innerHTML = '';
     
     addMines()
     
-    for (let r = 0; r < rows; r++)
-    {
+    for (let r = 0; r < rows; r++){
         let row = [];
-        for (let c = 0; c < columns; c++)
-        {
+        for (let c = 0; c < columns; c++){
             let tile = document.createElement('div'); 
             tile.id = r.toString() + '-' + c.toString(); 
             tile.addEventListener('click', tileClicked); 
-            tile.oncontextmenu = function(e)
-            {
+            tile.oncontextmenu = function(e){
                 e.preventDefault();
                 addFlag(tile);
             }
@@ -135,95 +131,73 @@ function createBoard()
         }
         board.push(row);
     }
-
-    
-
-    console.log(board)
 }
 
-function enableFlagButton()
-{
-    if(enableFlag)
-    {
+//For mobile users. It allows you to enable the flag that you can put on mines
+function enableFlagButton(){
+    if(enableFlag){
         enableFlag = false;
         document.querySelector('#flag-button').style.backgroundColor = 'lightgray';
-    }
-
-    else
-    {
+    }else{
         enableFlag = true;
         document.querySelector('#flag-button').style.backgroundColor = 'darkgray';
     }
 }
 
-function addFlag(tile)
-{
-    if (gameOver || tile.classList.contains('clicked'))
-    {
+//Puts a flag icon/removes it from the tile
+function addFlag(tile){
+    if (gameOver || tile.classList.contains('clicked')){
         return;
     }
-
-    if (!tile.classList.contains('flag'))
-    {
+    if (!tile.classList.contains('flag')){
         tile.classList.add('flag');
         tile.innerHTML = "<i class='fa-solid fa-flag'>";
-    }    
-
-    else
-    {
+    }else{
         tile.classList.remove('flag');
         tile.innerHTML = '';
     }
 }
 
-
-function tileClicked()
-{
-    if (gameOver || this.classList.contains('clicked'))
-    {
+//Check the tile on click
+function tileClicked(){
+    //If the tile was already clicked or the game is over do nothing
+    if (gameOver || this.classList.contains('clicked')){
         return;
     }
 
     let tile = this;
-
-    if(enableFlag)
-    {
-        if (tile.innerText == '')
-        {
+    
+    //If the flag button is enabled and there is no flag on a tile: create one. Else remove it.
+    if(enableFlag){
+        if (tile.innerText == ''){
             tile.innerHTML = "<i class='fa-solid fa-flag'>";    
-        }
-
-        else if (tile.innerHTML == "<i class='fa-solid fa-flag")
-        {
+        }else if (tile.innerHTML == "<i class='fa-solid fa-flag"){
             tile.innerHTML = '';
         }
-
         return;
     }
 
-    if (mineLocation.includes(tile.id))
-    {
+    //If you clicked on a mine set gameOver to true and reveal all the mines
+    if (mineLocation.includes(tile.id)){
         gameOver = true;
         document.getElementById('game_state').innerText = 'Game Over!';
         revealMinesLocation();
         return;
     }
 
+    //Split mine id to create coordinates array. Call mineCheck function
     let coordinates = tile.id.split('-');
     let r = parseInt(coordinates[0]);
     let c = parseInt(coordinates[1]);
     mineCheck(r, c);
 }
 
-function revealMinesLocation()
-{
-    for (let r = 0; r < rows; r++)
-    {
-        for (let c = 0; c < columns; c++)
-        {
+//Show all the mines and change the background color on mine tiles to red
+function revealMinesLocation(){
+    for (let r = 0; r < rows; r++){
+        for (let c = 0; c < columns; c++){
             let tile = board[r][c];
-            if(mineLocation.includes(tile.id))
-            {
+            if(mineLocation.includes(tile.id)){
                 tile.innerText = '';
                 bombIcon = document.createElement('i');
                 bombIcon.classList.add('fa-solid', 'fa-bomb');
@@ -235,15 +209,14 @@ function revealMinesLocation()
     }
 }
 
-function mineCheck(r, c)
-{
-    if (r < 0 || r >= rows || c < 0 || c >= columns)
-    {
+
+//Check if there are mines around a tile you clicked. Reveal the tile if there is no mines next to a tile
+function mineCheck(r, c){
+    if (r < 0 || r >= rows || c < 0 || c >= columns){
         return;
     }
 
-    if (board[r][c].classList.contains('clicked'))
-    {
+    if (board[r][c].classList.contains('clicked')){
         return;
     }
 
@@ -265,45 +238,33 @@ function mineCheck(r, c)
     found += tileCheck(r+1, c); 
     found += tileCheck(r+1, c+1); 
 
-    if (found>0)  
-    {
+    if (found>0)  {
         board[r][c].innerText = found; 
         board[r][c].classList.add('number' + found.toString()); 
-    }
-
-    else 
-    { 
-       
+    }else { 
         mineCheck(r-1, c-1);   
         mineCheck(r-1, c);      
         mineCheck(r-1, c+1);   
 
-      
         mineCheck(r, c-1);     
         mineCheck(r, c+1);     
-
       
         mineCheck(r+1, c-1);   
         mineCheck(r+1, c);      
         mineCheck(r+1, c+1);    
     }
 
-    if (clicked == rows * columns - mines)
-    {
+    //If you revealed all the tiles without a mine set a game state to win and reveal the mines
+    if (clicked == rows * columns - mines){
         document.getElementById('game_state').innerText = 'You won!';
-        for (let r = 0; r < rows; r++)
-        {
-        for (let c = 0; c < columns; c++)
-        {
+        for (let r = 0; r < rows; r++){
+        for (let c = 0; c < columns; c++){
             let tile = board[r][c];
-            if(mineLocation.includes(tile.id))
-            {
+            if(mineLocation.includes(tile.id)){
                 tile.innerText = '';
                 bombIcon = document.createElement('i');
                 bombIcon.classList.add('fa-solid', 'fa-bomb');
                 tile.appendChild(bombIcon);
-
-
                 tile.style.backgroundColor = 'rgba(29, 248, 146, 0.699)';
             }
         }
